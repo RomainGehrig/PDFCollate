f0455f91e7d56b731a51bc329db6fc76bb22615f
Romain Gehrig <romain.gehrig@gmail.com>
2021-10-21 14:15:32 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/RomainGehrig/PDFCollate'
 EMAIL = 'romain.gehrig@gmail.com'
 AUTHOR = 'Romain Gehrig'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
